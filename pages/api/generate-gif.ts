813fa8c17f14e57d712c659dd10d7ff575a7cfe4
gaoying <gaoying@9fbank.cc>
2024-10-30 11:03:22 +0800

    png to git

---
+++ pages/api/generate-gif.ts
@@ -0,0 +1,81 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import GIFEncoder from 'gifencoder';
+import { createCanvas, loadImage } from 'canvas';
+import path from 'path'
+
+interface RequestBody {
+  imageUrls: string[]; // Array of base64 encoded images
+}
+
+// // 定义允许的 HTTP 方法
+// export const dynamic = 'force-dynamic';
+
+// // 处理 OPTIONS 请求（用于 CORS）
+// export async function OPTIONS() {
+//     return new NextResponse(null, {
+//         status: 200,
+//         headers: {
+//             'Access-Control-Allow-Methods': 'POST',
+//             'Access-Control-Allow-Headers': 'Content-Type',
+//         },
+//     });
+// }
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests are allowed' });
+    return;
+  }
+
+  const { imageUrls } = req.body as RequestBody;
+
+  console.log('----- imageUrls', req.body, imageUrls)
+
+  if (!imageUrls || imageUrls.length !== 3) {
+    res.status(400).send({ message: 'Please provide exactly three images.' });
+    return;
+  }
+
+  try {
+    // 创建 GIF 编码器
+    const encoder = new GIFEncoder(217, 217); // 设置 GIF 尺寸
+    
+    // 创建输出流
+    // encoder.createReadStream();
+    // const chunks: any[] = [];
+    
+    // buffer.on('data', (chunk) => chunks.push(chunk));
+    
+    // 设置 GIF 参数
+    encoder.setRepeat(0);   // 0 表示循环播放
+    encoder.setDelay(500);  // 每帧延迟 500ms
+    encoder.setQuality(10); // 图片质量
+    encoder.start();
+
+    
+    // 创建 canvas
+    const canvas = createCanvas(217, 217);
+    const ctx  = canvas.getContext('2d');
+    
+    // 处理每张图片
+    for (const url of imageUrls) {
+      const publicPath = path.join(process.cwd(), 'public', url.split('/').pop()!)
+      const image = await loadImage(publicPath)
+      ctx.drawImage(image, 0, 0, 217, 217);
+      encoder.addFrame(ctx as unknown as CanvasRenderingContext2D);
+    }
+    
+    encoder.finish();
+
+    const buffer = encoder.out.getData();
+    
+    // 将 buffer 转换为 base64
+    // const buffer64 = Buffer.concat(chunks).toString('base64');
+    
+    res.setHeader('Content-Type', 'image/gif');
+    res.status(200).send(buffer);
+  } catch (error){
+    console.error(error);
+    res.status(500).json({ message: 'Error generating GIF', error });
+  }
+}
---
+++ pages/components/GiftGenerator.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+
+import Image from "next/image";
+import { useState } from 'react';
+
+export default function GifGenerator() {
+  const [ isLoading, setIsLoading ] = useState(false)
+  const [ gifUrl, setGifUrl ] = useState('')
+
+  const imageUrls = [
+    `/camera2.png`,
+    `/contact.png`,
+    `/doc.png`
+  ]
+
+  const generateGift = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch('/api/generate-gif', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          imageUrls
+        })
+      })
+
+      const blob = await response.blob();
+      console.log('----- generateGift blob', blob)
+      const url = URL.createObjectURL(blob);
+      setGifUrl(url);
+
+      // const data = await response.json();
+      // if (data.gif) {
+      //   setGifUrl(data.gif);
+      // } else {
+      //   console.error('Failed to generate GIF');
+      // }
+
+    } catch (error) {
+      console.error('Error generating GIF:', error)
+    } finally {
+      setIsLoading(false)
+    }
+    
+  }
+
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <button onClick={generateGift} className="bg-white text-black w-[100px] h-[50px]">
+        {
+          isLoading ? '生成中...' : '生成'
+        }
+        </button>
+
+        {
+          gifUrl && (<Image
+            className="dark:invert"
+            src={gifUrl}
+            alt="gift"
+            width={180}
+            height={38}
+            priority
+          />)
+        }
+       
+      </main>
+      
+    </div>
+  );
+}
